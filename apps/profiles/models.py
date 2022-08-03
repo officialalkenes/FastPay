bbb42b087b99ab1d9fc673e44cd227a48acbca5d
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-08-03 12:37:06 +0100

    chore:update profiles information

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -20,10 +20,17 @@ class Profile(models.Model):
         male = ('Male', 'Male')
         female = ('female', 'female')
         unspecified = ('Unspecified', 'Unspecified')
+
     class ValidIdOptions(models.TextChoices):
         nin = ('NIN', 'NIN')
         voters_card = ('Voters Card', 'Voters Card')
         passport = ("International Passport", 'International Passport')
+
+    class KycLevelOptions(models.IntegerChoices):
+        Level1 = (1, 'Level 1 Registration')
+        Level2 = (2, 'Level 2 Registration')
+        Level3 = (3, 'Level 3 Registration')
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     profile_pics = models.ImageField(upload_to=upload_to, verbose_name=_("Profile Picture"))
     gender = models.CharField(max_length=12, verbose_name=_("Gender"), choices=GenderOptions,
@@ -40,13 +47,28 @@ class Profile(models.Model):
                                             blank=True, null=True)
     account_number = models.IntegerField(verbose_name=_("Account Number"), unique=True,
                                          blank=True, null=True)
+    residential_address = models.CharField(max_length=100, verbose_name=_("Residential Address"),
+                                           blank=True)
+    lga = models.models.CharField(max_length=100, verbose_name=_("Local Government Area"),
+                                           blank=True)
+    state = models.models.CharField(max_length=100, verbose_name=_("State"),
+                                           blank=True)
+    upgraded_status = models.BooleanField(default=False,
+                            help_text=_("True If all user's Information are valid and not null"),
+                            verbose_name=_("Status Update"))
+    kyc_level = models.IntegerField(verbose_name=_("Kyc Level Verification"),
+                                 choices=KycLevelOptions)
+
+
     creation_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
+
+
 
     def __str__(self) -> str:
         return f'{self.user.username}'
 
     def save(self, *args, **kwargs):
         if not self.account_number:
-            self.account_number = ''
+            self.account_number = int(self.user.phone_number[2:])
         return super().save(*args, **kwargs)
