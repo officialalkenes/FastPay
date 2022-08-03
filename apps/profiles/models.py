60dfbc2fa4d06eb7528f8dc29ef1d1c144075b7c
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-08-03 17:13:18 +0100

    chore: Create the Kyc Model for User

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+from tokenize import blank_re
 from django.contrib.auth import get_user_model
 
 from django.db import models
@@ -10,26 +11,31 @@ from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
+
+# class ImageUploadPath:
+#     def __init__(self) -> None:
+#         username = self.instance.user
+
+
 def upload_to(instance, filename):
     username = instance.user
     today = datetime.today()
-    return f"profile/{username}/filename.jpg"
+    return f"profile/{username}/{filename}.jpg"
+
+def kyc_upload_front(instance, filename):
+    username = instance.user
+    return f'kyc/front/{username}/{filename}.jpg'
+
+def kyc_upload_back(instance, filename):
+    username = instance.user
+    return f'kyc/back/{username}/{filename}.jpg'
+
 
 class Profile(models.Model):
     class GenderOptions(models.TextChoices):
         male = ('Male', 'Male')
         female = ('female', 'female')
         unspecified = ('Unspecified', 'Unspecified')
-
-    class ValidIdOptions(models.TextChoices):
-        nin = ('NIN', 'NIN')
-        voters_card = ('Voters Card', 'Voters Card')
-        passport = ("International Passport", 'International Passport')
-
-    class KycLevelOptions(models.IntegerChoices):
-        Level1 = (1, 'Level 1')
-        Level2 = (2, 'Level 2')
-        Level3 = (3, 'Level 3')
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     profile_pics = models.ImageField(upload_to=upload_to, verbose_name=_("Profile Picture"))
@@ -40,11 +46,6 @@ class Profile(models.Model):
                                              help_text=_("User's Bank Verification Number"),
                                              blank=True)
     date_of_birth = models.DateField(blank=True, verbose_name="Date of Birth")
-    valid_id = models.CharField(max_length=20, verbose_name=_("Valid Identity Card"),
-                                help_text=_("Select a Valid card you own from the options"),
-                                choices=ValidIdOptions, blank=True)
-    id_num = models.PositiveBigIntegerField(verbose_name=_("Id Unique Digits"),
-                                            blank=True, null=True)
     account_number = models.IntegerField(verbose_name=_("Account Number"), unique=True,
                                          blank=True, null=True)
     residential_address = models.CharField(max_length=100, verbose_name=_("Residential Address"),
@@ -53,13 +54,6 @@ class Profile(models.Model):
                                            blank=True)
     state = models.models.CharField(max_length=100, verbose_name=_("State"),
                                            blank=True)
-    upgraded_status = models.BooleanField(default=False,
-                            help_text=_("True If all user's Information are valid and not null"),
-                            verbose_name=_("Status Update"))
-    kyc_level = models.IntegerField(verbose_name=_("Kyc Level Verification"),
-                                 choices=KycLevelOptions)
-
-
     creation_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
@@ -72,3 +66,39 @@ class Profile(models.Model):
         if not self.account_number:
             self.account_number = int(self.user.phone_number[2:])
         return super().save(*args, **kwargs)
+
+
+class KycVerification(models.Model):
+    class ValidIdOptions(models.TextChoices):
+        nin = ('NIN', 'NIN')
+        voters_card = ('Voters Card', 'Voters Card')
+        passport = ("International Passport", 'International Passport')
+
+    class KycLevelOptions(models.IntegerChoices):
+        Level1 = (1, 'Level 1')
+        Level2 = (2, 'Level 2')
+        Level3 = (3, 'Level 3')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name=_("kyc"))
+    valid_id = models.CharField(max_length=20, verbose_name=_("Valid Identity Card"),
+                                help_text=_("Select a Valid card you own from the options"),
+                                choices=ValidIdOptions, blank=True)
+    id_num = models.PositiveBigIntegerField(verbose_name=_("Id Unique Digits"),
+                                            blank=True, null=True)
+    front_image = models.ImageField(upload_to=kyc_upload_front, verbose_name=_("Id Front Image"),
+                                    help_text=_("format -> jpg, Front Image of Id"), blank=True)
+    back_image = models.ImageField(upload_to=kyc_upload_front, verbose_name=_("Id Back Image"),
+                                    help_text=_("format -> jpg, Back Image of Id"), blank=True)
+    upgraded_status = models.BooleanField(default=False,
+                            help_text=_("True If all user's Information are valid and not null"),
+                            verbose_name=_("Status Update"))
+    kyc_level = models.IntegerField(verbose_name=_("Kyc Level Verification"),
+                                 choices=KycLevelOptions, default=KycLevelOptions.1)
+
+    def __str__(self) -> str:
+        return f'{self.user.username}'
+
+    class Meta:
+        verbose_name = _("Kyc Information")
+        verbose_name_plural = _("Kyc Information")
