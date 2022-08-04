ee9eb337421b63d9781ff5b5da65001ea8b34aff
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-08-04 09:28:50 +0100

    chore:create the  api apps

---
+++ apps/api/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+class BillingRecordsView(generics.ListAPIView):
+    queryset = Billing.objects.all()
+    serializer_class = BillingRecordsSerializer
+    pagination_class = LargeResultsSetPagination
